257d8458b06f7cbc9733372f54b4c3c00ef7e2de
Stag06 <stag006@benner.com.br>
2019-06-19 11:40:32 -0300

    primeiro envio do masterial-MapaMental e Dia

---
+++ Orientacao-Padrinho/Mapa Mental/Mapa Mental Controle Estoque Obras.mm
@@ -0,0 +1,164 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1559824969343" ID="ID_1716295153" MODIFIED="1560770613059" TEXT="Controle Estoque Obras">
+<font BOLD="true" ITALIC="true" NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="launch"/>
+<icon BUILTIN="idea"/>
+<node CREATED="1559825086648" ID="ID_1564467021" MODIFIED="1560778744880" POSITION="right" TEXT="Administrativo">
+<node CREATED="1559825157158" ID="ID_1532745486" MODIFIED="1560771796305" TEXT="Engenheiro">
+<icon BUILTIN="male1"/>
+<icon BUILTIN="full-5"/>
+<node CREATED="1559826691541" ID="ID_1996904245" MODIFIED="1559826725612" TEXT="Or&#xe7;amentos das Etapas">
+<icon BUILTIN="full-2"/>
+<node CREATED="1559827102814" ID="ID_458573888" MODIFIED="1559827241304">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Categoria
+    </p>
+    <p>
+      &#160;&#160;+ Orcamento Quantidade
+    </p>
+    <p>
+      &#160;&#160;+ Orcamento Valor
+    </p>
+    <p>
+      &#160;
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+<node CREATED="1560770416600" ID="ID_502493136" MODIFIED="1560771609006" TEXT=" Etapas Projetos">
+<icon BUILTIN="full-5"/>
+<node CREATED="1559827102814" ID="ID_1455273909" MODIFIED="1560771726246">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      + Inicia&#231;&#227;o
+    </p>
+    <p>
+      + Planejamento
+    </p>
+    <p>
+      + Controle e Monitoramento
+    </p>
+    <p>
+      + Encerramento
+    </p>
+    <p>
+      &#160;
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+</node>
+<node CREATED="1559825167766" ID="ID_1643375331" MODIFIED="1559826300259" TEXT="Escrit&#xf3;rio(Diretoria)">
+<icon BUILTIN="family"/>
+<node CREATED="1559825386283" ID="ID_1839710361" MODIFIED="1559826086479" TEXT="Cadastrar Pessoas">
+<icon BUILTIN="pencil"/>
+<icon BUILTIN="full-1"/>
+<node CREATED="1559825421497" ID="ID_671431014" MODIFIED="1559825702566" TEXT="+Dados Pessoas"/>
+<node CREATED="1559825472218" ID="ID_411856527" MODIFIED="1559825742274" TEXT="+Capataz de Obra (Logica)"/>
+<node CREATED="1559825503049" ID="ID_302279103" MODIFIED="1559825748197" TEXT="+Ativo (Logica)"/>
+</node>
+</node>
+<node CREATED="1559825291964" ID="ID_45114994" MODIFIED="1559826605491" TEXT="Cadastros">
+<node CREATED="1559825302332" ID="ID_1583459853" MODIFIED="1559826091078" TEXT="Categoria Ferramentas">
+<icon BUILTIN="pencil"/>
+<icon BUILTIN="full-1"/>
+<node CREATED="1559825567848" ID="ID_935758149" MODIFIED="1559825716446" TEXT="Nome"/>
+<node CREATED="1559825579607" ID="ID_196612424" MODIFIED="1559825714085" TEXT="Descri&#xe7;&#xe3;o"/>
+</node>
+<node CREATED="1559825316684" FOLDED="true" ID="ID_51317721" MODIFIED="1559826862901" TEXT="Categoria Insumos">
+<icon BUILTIN="pencil"/>
+<icon BUILTIN="full-1"/>
+<node CREATED="1559825520872" ID="ID_379997029" MODIFIED="1559825712038" TEXT="Nome"/>
+<node CREATED="1559825531568" ID="ID_1112092404" MODIFIED="1559825710301" TEXT="Descri&#xe7;&#xe3;o"/>
+</node>
+<node CREATED="1559826607654" ID="ID_1634323968" MODIFIED="1559826623982" TEXT="Cadastrar Projetos">
+<icon BUILTIN="pencil"/>
+<icon BUILTIN="full-1"/>
+</node>
+</node>
+</node>
+<node CREATED="1559825118847" ID="ID_600477369" MODIFIED="1560770009642" POSITION="left" TEXT="Almoxarife">
+<icon BUILTIN="male1"/>
+<node CREATED="1559825209326" ID="ID_500327218" MODIFIED="1559825217518" TEXT="Cadastros">
+<node CREATED="1559825225311" ID="ID_1994920517" MODIFIED="1559826080111" TEXT="Cadastrar Insumos">
+<icon BUILTIN="pencil"/>
+<icon BUILTIN="full-1"/>
+<node CREATED="1559825810900" ID="ID_703914855" MODIFIED="1559825831940" TEXT="Dados Insumo"/>
+<node CREATED="1559825848619" ID="ID_851429627" MODIFIED="1559825863745" TEXT="Categoria Insumo"/>
+</node>
+<node CREATED="1559825274061" ID="ID_344393142" MODIFIED="1559826082447" TEXT="Cadastrar Ferramentas">
+<icon BUILTIN="pencil"/>
+<icon BUILTIN="full-1"/>
+<node CREATED="1559826017440" ID="ID_400600689" MODIFIED="1559826067119" TEXT="Dados Ferramenta"/>
+<node CREATED="1559826050240" ID="ID_1066859504" MODIFIED="1559826060942" TEXT="Categoria Ferramenta"/>
+</node>
+</node>
+<node CREATED="1559826356251" ID="ID_668245191" MODIFIED="1560778797152" TEXT="Atividades">
+<arrowlink DESTINATION="ID_668245191" ENDARROW="Default" ENDINCLINATION="0;0;" ID="Arrow_ID_943639670" STARTARROW="None" STARTINCLINATION="0;0;"/>
+<linktarget COLOR="#b0b0b0" DESTINATION="ID_668245191" ENDARROW="Default" ENDINCLINATION="0;0;" ID="Arrow_ID_943639670" SOURCE="ID_668245191" STARTARROW="None" STARTINCLINATION="0;0;"/>
+<icon BUILTIN="full-2"/>
+<node CREATED="1559826375122" ID="ID_120111398" MODIFIED="1559826391611" TEXT="Solicita&#xe7;&#xe3;o Compras"/>
+<node CREATED="1559826400066" ID="ID_1987955693" MODIFIED="1559826412378" TEXT="Entrada Insumos"/>
+<node CREATED="1559826417753" ID="ID_400151553" MODIFIED="1559826423002" TEXT="Saida Insumos"/>
+<node CREATED="1559826428961" ID="ID_1942651974" MODIFIED="1559826435386" TEXT="Entrada Ferramentas"/>
+<node CREATED="1559826437946" ID="ID_1686008025" MODIFIED="1559826446730" TEXT="Saida Ferramentas"/>
+<node CREATED="1559826504329" ID="ID_539893493" MODIFIED="1559826514616" TEXT="Relatorios"/>
+</node>
+</node>
+<node CREATED="1559826729814" ID="ID_310512450" MODIFIED="1560770527226" POSITION="right" TEXT="Capataz">
+<icon BUILTIN="full-5"/>
+<node CREATED="1559826810563" ID="ID_839058605" MODIFIED="1559826824354" TEXT="Gest&#xe3;o M&#xe3;o Entrada">
+<icon BUILTIN="stop-sign"/>
+</node>
+<node CREATED="1559828247739" ID="ID_1733189808" MODIFIED="1559828262050" TEXT="Transferencia">
+<icon BUILTIN="help"/>
+</node>
+</node>
+<node CREATED="1559827342898" ID="ID_808088590" MODIFIED="1559827580603" POSITION="left" TEXT="Pensar">
+<icon BUILTIN="help"/>
+<node CREATED="1559827364114" ID="ID_648801994" MODIFIED="1559827542995">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Quem s&#227;o as pessoas
+    </p>
+    <p>
+      &#160;&#160;Engenheiro, Capataz, Funcion&#225;rio Escritorio, Almoxarife
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      Quais s&#227;o as funcoes de cada Pessoas no sistema
+    </p>
+    <p>
+      Quais s&#227;o as informa&#231;&#245;es que cada pessoa vai querer obter do sistema
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+<node CREATED="1560770614809" ID="ID_1620268843" MODIFIED="1560770831358" POSITION="right" TEXT="Tecnicas">
+<icon BUILTIN="freemind_butterfly"/>
+<node CREATED="1560770645849" ID="ID_1869821515" MODIFIED="1560770658256" TEXT="TDD"/>
+<node CREATED="1560770729814" ID="ID_474739590" MODIFIED="1560770825365" TEXT="UML de Classe "/>
+<node CREATED="1560770736813" ID="ID_654572424" MODIFIED="1560770830037" TEXT=" Data Base"/>
+<node CREATED="1560770834804" ID="ID_1278376131" MODIFIED="1560770866480" TEXT="Parcial Class"/>
+</node>
+</node>
+</map>
